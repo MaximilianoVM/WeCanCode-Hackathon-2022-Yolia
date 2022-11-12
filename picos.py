590f1bac050fab0c54592150997d1a70eab0f073
Alondra <avaldez34@uabc.edu.mx>
2022-11-12 01:03:08 -0800

    picos
    agrega tipos de picos
    enredaderas y rosales, ambos hacen daño

--- picos.py
+++ picos.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Picos(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y,tipo):
         super().__init__()
 
         self.x = x
@@ -10,8 +10,16 @@ class Picos(pygame.sprite.Sprite):
         self.frames = []
         self.frame_index = 0
 
-        self.image = pygame.image.load('assets/Sprite-0006.png').convert_alpha()
+        if tipo=='rosales':
+            self.image.load('assets/rosales.png').convert_alpha()
+        elif tipo=='enredadera':
+            self.image.load('assets/Enredaderas/enredadera2.png').convert_alpha()
+        
+
+        #self.image = pygame.image.load('assets/Sprite-0006.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = (self.x, self.y))
+
+
 
     def collision(self, other):
          return self.rect.colliderect(other.rect)
