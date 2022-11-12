21bdce6967f6081f78b35cc081381e8755e21629
asaPUP <asael.silvam@gmail.com>
2022-11-11 23:32:50 -0800

    as

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from jugador import Jugador
 from pared import Pared
 from meta import Meta
+from picos import Picos
 from sys import exit
 
 pygame.init()
@@ -26,7 +27,7 @@ player1.add(Jugador(564, 124, False, 'mexica'))
 fondo_surface = pygame.image.load('assets/HACKATHON FONDO TEMP.png').convert()
 
 meta1 = Meta(52, 124)
-meta2 = Meta(WIDTH - 52, HEIGHT -20)
+meta2 = Meta(52 + 576 + 20, 124)
 
 while True: 
     for event in pygame.event.get(): 
@@ -37,8 +38,8 @@ while True:
     WIN.blit(fondo_surface, (0,0))
 
     WIN.blit(meta1.image, meta1.rect)
+    WIN.blit(meta2.image, meta2.rect)
     player1.draw(WIN)
 
     pygame.display.update()
     clock.tick(60)
-        
